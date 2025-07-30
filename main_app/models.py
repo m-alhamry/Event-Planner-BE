9e712544bf2d3bc775b6e57d8b90a95ea9b99681
alhamrimohamed <alhamrimohammed4@gmail.com>
2025-07-30 11:59:32 +0300

    create Event and Attendee models

--- main_app/models.py
+++ main_app/models.py
@@ -12,3 +12,30 @@ class UserProfile(models.Model):
     
     class Meta:
         db_table = 'user_profile'
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_events')
+    
+    def __str__(self):
+        return f"Event: {self.title} - Created by {self.created_by.username} on {self.date.strftime('%Y-%m-%d')} at {self.time.strftime('%H:%M')}"
+    
+    class Meta:
+        db_table = 'event'
+        ordering = ['-date', '-time']
+
+class Attendee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attendees')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='attendances')
+    confirmed = models.BooleanField(default=False)    
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
+    
+    class Meta:
+        db_table = 'attendee'
+        unique_together = ('user', 'event')  # Ensure a user can only register for an event once
