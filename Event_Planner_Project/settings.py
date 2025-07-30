9536f016633b0be722de98ff649f063dccd88f65
alhamrimohamed <alhamrimohammed4@gmail.com>
2025-07-30 09:27:50 +0300

    database configuration

--- Event_Planner_Project/settings.py
+++ Event_Planner_Project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Event_Planner_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event_planner',  # Name of your PostgreSQL database
+        'USER': 'postgres',
+        'PASSWORD': '1@Albahrain',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
 
