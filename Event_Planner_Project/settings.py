257f16fa4606fc31920e2944299961d7e873534c
alhamrimohamed <alhamrimohammed4@gmail.com>
2025-08-06 15:14:12 +0300

    update

--- Event_Planner_Project/settings.py
+++ Event_Planner_Project/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
 
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
+# CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
 
 from datetime import timedelta
 
